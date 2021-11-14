b8fe78d2af9827d5cd6476585050c3f8bee9a236
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-11-14 17:24:58 +0700

    feat: setup Babel

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ const gulpHTMLMin = require('gulp-htmlmin')
 const gulpAutoprefixer = require('gulp-autoprefixer')
 const gulpCleanCSS = require('gulp-clean-css')
 const gulpUglify = require('gulp-uglify')
+const gulpBabel = require('gulp-babel')
 
-function cleanDist(callback) {
+function clean(callback) {
 	return gulp
 		.src('dist', {
 			read: false,
@@ -40,8 +41,22 @@ function processCSS(callback) {
 }
 
 function processJS(callback) {
-	return gulp.src('src/**/*.js').pipe(gulpUglify()).pipe(gulp.dest('dist')).on('end', callback)
+	return gulp
+		.src('src/**/*.js')
+		.pipe(
+			gulpBabel({
+				presets: ['@babel/env'],
+			})
+		)
+		.pipe(gulpUglify())
+		.pipe(gulp.dest('dist'))
+		.on('end', callback)
 }
 
-exports.clean = cleanDist
-exports.build = gulp.series(cleanDist, gulp.parallel(copySrc, processHTML, processCSS, processJS))
+function build(callback) {
+	return gulp.parallel(copySrc, processHTML, processCSS, processJS)(callback)
+}
+
+exports.clean = clean
+exports.build = build
+exports.default = gulp.series(clean, build)
