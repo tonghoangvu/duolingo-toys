1f369c096a20ca50f2e64855c8701fb3a9e46a0b
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-11-22 11:46:21 +0700

    chore: update options page for i18n support

--- src/js/util.js
+++ src/js/util.js
@@ -4,3 +4,14 @@ async function getOption(name) {
 	const data = await chrome.storage.sync.get(name)
 	return data[name]
 }
+
+function applyLocalization() {
+	const langElements = document.querySelectorAll('.lang')
+	langElements.forEach(
+		element => (element.textContent = getLangMessage(element.textContent.trim()))
+	)
+}
+
+function getLangMessage(value) {
+	return chrome.i18n.getMessage(value)
+}
